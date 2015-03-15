7d8c37b48c6125b39b15b6cc54495eaf6480ddbf
Martin <zhuec@hotmail.com>
2015-03-15 18:34:43 +0800

    LuaLib
    v 1.0

---
+++ LuaLib/CLua.cpp
@@ -0,0 +1,161 @@
+#include <windows.h>
+#include <cLua.h>
+#include <string>
+
+cLua::cLua() {
+    m_pErrorHandler = NULL;
+    m_pScriptContext = luaL_newstate();
+    luaL_openlibs(m_pScriptContext);
+}
+
+cLua::~cLua() {
+    if (m_pScriptContext) {
+        lua_close(m_pScriptContext);
+    }
+}
+
+static std::string findScript(const char* pFname) {
+    FILE* fTest;
+
+    char drive[_MAX_DRIVE + 1];
+    char dir[_MAX_DIR + 1];
+    char fname[_MAX_FNAME + 1];
+    char ext[_MAX_EXT + 1];
+
+    _splitpath_s(pFname, drive, _MAX_DRIVE, dir, _MAX_DIR, fname, _MAX_FNAME, ext, _MAX_EXT);
+
+    std::string strTestFile = (std::string) drive + dir + "Scripts\\" + fname + ".LUB";
+    int nRet = fopen_s(&fTest, strTestFile.c_str(), "r"); // ³É¹¦·µ»Ø0
+    if (nRet != 0) {
+        //not that one...
+        strTestFile = (std::string) drive + dir + "Scripts\\" + fname + ".LUA";
+        nRet = fopen_s(&fTest, strTestFile.c_str(), "r");
+    }
+
+    if (nRet != 0) {
+        //not that one...
+        strTestFile = (std::string) drive + dir + fname + ".LUB";
+        nRet = fopen_s(&fTest, strTestFile.c_str(), "r");
+    }
+
+    if (nRet != 0) {
+        //not that one...
+        //not that one...
+        strTestFile = (std::string) drive + dir + fname + ".LUA";
+        nRet = fopen_s(&fTest, strTestFile.c_str(), "r");
+    }
+
+    if (nRet == 0) {
+        fclose(fTest);
+    }
+
+    return strTestFile;
+}
+
+bool cLua::RunScript(const char* pFname) {
+    std::string strFilename = findScript(pFname);
+    const char* pFilename = strFilename.c_str();
+
+    if (0 != luaL_dofile(m_pScriptContext, pFilename)) {
+        if (m_pErrorHandler) {
+            char buf[256];
+            sprintf_s(buf, 255, "Lua Error - Script Load\nScript Name:%s\nError Message:%s\n", pFilename, luaL_checkstring(m_pScriptContext, -1));
+            m_pErrorHandler(buf);
+        }
+        return false;
+    }
+
+    //if (0 != luaL_loadfile(m_pScriptContext, pFilename)) {
+    //	if(m_pErrorHandler) {
+    //		char buf[256];
+    //		sprintf(buf, "Lua Error - Script Load\nScript Name:%s\nError Message:%s\n", pFilename, luaL_checkstring(m_pScriptContext, -1));
+    //		m_pErrorHandler(buf);
+    //	}
+    //	return false;
+    //}
+
+    //if (0 != lua_pcall(m_pScriptContext, 0, LUA_MULTRET, 0)) {
+    //	if(m_pErrorHandler) {
+    //		char buf[256];
+    //		sprintf(buf, "Lua Error - Script Run\nScript Name:%s\nError Message:%s\n", pFilename, luaL_checkstring(m_pScriptContext, -1));
+    //		m_pErrorHandler(buf);
+    //	}
+    //	return false;
+    //}
+
+    return true;
+
+}
+
+bool cLua::RunString(const char* pCommand) {
+    if (0 != luaL_dostring(m_pScriptContext, pCommand)) {
+        if (m_pErrorHandler) {
+            char buf[256];
+            sprintf_s(buf, 255, "Lua Error - String Load\nString:%s\nError Message:%s\n", pCommand, luaL_checkstring(m_pScriptContext, -1));
+            m_pErrorHandler(buf);
+        }
+        return false;
+    }
+
+    //if (0 != luaL_loadbuffer(m_pScriptContext, pCommand, strlen(pCommand), NULL)) {
+    //    if (m_pErrorHandler) {
+    //        char buf[256];
+    //        sprintf_s(buf, 255, "Lua Error - String Load\nString:%s\nError Message:%s\n", pCommand, luaL_checkstring(m_pScriptContext, -1));
+    //        m_pErrorHandler(buf);
+    //    }
+    //    return false;
+    //}
+
+    //if (0 != lua_pcall(m_pScriptContext, 0, LUA_MULTRET, 0)) {
+    //    if (m_pErrorHandler) {
+    //        char buf[256];
+    //        sprintf_s(buf, 255, "Lua Error - String Run\nString:%s\nError Message:%s\n", pCommand, luaL_checkstring(m_pScriptContext, -1));
+    //        m_pErrorHandler(buf);
+    //    }
+    //    return false;
+    //}
+
+    return true;
+}
+
+const char* cLua::GetErrorString(void) {
+    return luaL_checkstring(m_pScriptContext, -1);
+}
+
+
+bool cLua::AddFunction(const char* pFunctionName, LuaFunctionType pFunction) {
+    lua_register(m_pScriptContext, pFunctionName, pFunction);
+    return true;
+}
+
+const char* cLua::GetStringArgument(int num, const char* pDefault) {
+    return luaL_optstring(m_pScriptContext, num, pDefault);
+}
+
+double cLua::GetNumberArgument(int num, double dDefault) {
+    return luaL_optnumber(m_pScriptContext, num, dDefault);
+}
+
+int cLua::GetIntArgument(int num, int nDefault) {
+    return luaL_optinteger(m_pScriptContext, num, nDefault);
+}
+
+void cLua::PushString(const char* pString) {
+    lua_pushstring(m_pScriptContext, pString);
+}
+
+void cLua::PushNumber(double value) {
+    lua_pushnumber(m_pScriptContext, value);
+}
+
+void cLua::PushInt(int value) {
+    lua_pushinteger(m_pScriptContext, value);
+}
+
+void cLua::SetErrorHandler(void(*pErrHandler)(const char* pError)) {
+    m_pErrorHandler = pErrHandler;
+}
+
+lua_State* cLua::GetScriptContext(void) {
+    return m_pScriptContext;
+}
---
+++ LuaLib/CLua.h
@@ -0,0 +1,42 @@
+#pragma once
+
+extern "C" {
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+#include "luaconf.h"
+}
+
+#define LuaGlue extern "C" int  // 定义 LuaGlue 宏
+
+extern "C" { // 定义一个 C 方式的 LuaFunctionType 类型函数指针
+    typedef int(*LuaFunctionType)(lua_State *pLuaState);
+};
+
+class cLua {
+public:
+    cLua();
+    virtual ~cLua();
+
+    bool		RunScript(const char* pFilename);
+    bool		RunString(const char* pCommand);
+    const char* GetErrorString(void);
+
+    bool		AddFunction(const char* pFunctionName, LuaFunctionType pFunction);
+
+    const char* GetStringArgument(int num, const char* pDefault = NULL);
+    double		GetNumberArgument(int num, double dDefault = 0.0);
+    int 		GetIntArgument(int num, int nDefault = 0);
+
+    void		PushString(const char* pString);
+    void		PushNumber(double value);
+    void		PushInt(int value);
+
+    void		SetErrorHandler(void(*pErrHandler)(const char* pError));
+
+    lua_State*  GetScriptContext(void);
+
+private:
+    lua_State*  m_pScriptContext;
+    void(*m_pErrorHandler)(const char *pError);
+};
